0c870b41d58def02a72ce8b877d060335d47541e
Michael Sales <michael.fernandes.sales@gmail.com>
2021-12-07 18:19:53 -0300

    Definindo Rotas

--- routes/dish.routes.js
+++ routes/dish.routes.js
@@ -1,79 +1,77 @@
+const isAuthenticated = require("../middlewares/isAuthenticated");
 const express = require("express");
 const router = express.Router();
-const DishModel = require("../models/Dish.model");
+const ProductModel = require("../models/Product.model");
 
-router.post("/dish", async (req, res) => {
-    try {
-      console.log(req.body);        
-      const result = await DishModel.create(req.body);        
-      res.status(201).json(result);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+router.post("/product", isAuthenticated, async (req, res) => {
+  try {
+    console.log(req.body);
+    const result = await ProductModel.create(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/product", isAuthenticated, async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+
+    res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/product/:id", isAuthenticated, async (req, res) => {
+  try {
+    const product = await ProductModel.findOne({ _id: req.params.id });
+
+    if (!product) {
+      return res.status(404).json({ msg: "Produto não encontrado." });
     }
-  });
 
-  router.get("/dish", async (req, res) => {
-    try {
-      
-      const dishs = await DishModel.find();
-  
-      res.status(200).json(dishs);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.patch("/product/:id", isAuthenticated, async (req, res) => {
+  try {
+    const result = await ProductModel.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!result) {
+      return res.status(404).json({ msg: "Produto não encontrado." });
     }
-  });
 
-  router.get("/dish/:id", async (req, res) => {
-    try {
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-      const dish = await DishModel.findOne({ _id: req.params.id });
-  
-      if (!dish) {
-        return res.status(404).json({ msg: "Prato não encontrado." });
-      }
-  
-      res.status(200).json(dish);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+router.delete("/product/:id", isAuthenticated, async (req, res) => {
+  try {
+    const result = await ProductModel.deleteOne({ _id: req.params.id });
+
+    if (result.deletedCount < 1) {
+      return res.status(404).json({ msg: "Não encontrado" });
     }
-  });
 
-  router.patch("/dish/:id", async (req, res) => {
-    try {
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-      const result = await DishModel.findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: req.body },
-        { new: true, runValidators: true }
-      );
-  
-      if (!result) {
-        return res.status(404).json({ msg: "Prato não encontrado." });
-      }
-  
-      res.status(200).json(result);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-
-  router.delete("/dish/:id", async (req, res) => {
-    try {
-      const result = await DishModel.deleteOne({ _id: req.params.id });
-  
-      if (result.deletedCount < 1) {
-        return res.status(404).json({ msg: "Não encontrado" });
-      }
-
-      res.status(200).json({});
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  module.exports = router;
+module.exports = router;
