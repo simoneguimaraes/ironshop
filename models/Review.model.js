17e26e61a5f565d5da6322e4dff26a851c2200c4
Alissa Seixas <alissaps@gmail.com>
2021-12-07 15:23:00 -0300

    add isAdmin

--- models/Review.model.js
+++ models/Review.model.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     comment: { type: String, maxlength: 200 },
+    rate: { type: Number, enum: [1, 2, 3, 4, 5] },
     establishmentId: { type: Schema.Types.ObjectId, ref: "Establishment" },
   });
 
--- routes/establishment.routes.js
+++ routes/establishment.routes.js
@@ -1,20 +1,19 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
 const express = require("express");
 
 const UserModel = require("../models/User.model");
 const generateToken = require("../config/jwt.config");
-const isAuthenticated = require("../middlewares/isAuthenticated");
 const attachCurrentUser = require("../middlewares/attachCurrentUser");
 
-const salt_rounds = 10;
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const isAdmin = require("../middlewares/isAuthenticated");
 
 const EstablishmentModel = require("../models/Establishment.model");
 
 // CRUD
 
-// Crud Create (POST)
-router.post("/establishment",isAuthenticated, async (req, res) => {
+// Crud Create (POST) - só admin
+router.post("/establishment", isAuthenticated, isAdmin, async (req, res) => {
   try {
     console.log(req.body);
 
@@ -27,14 +26,19 @@ router.post("/establishment",isAuthenticated, async (req, res) => {
   }
 });
 
+// cRud Read (GET) (Lista) 
 
-// cRud Read (GET) (Lista)
-
-
-
-router.get("/establishment", async (req, res) => {
+router.get("/establishment", isAuthenticated, async (req, res) => {
   try {
-    const establishments = await EstablishmentModel.find();
+    const establishments = await EstablishmentModel.find()
+      .populate({
+        path: "products",
+        model: "Product",
+      })
+      .populate({
+        path: "reviews",
+        model: "Reviews",
+      });
 
     res.status(200).json(establishments);
   } catch (err) {
@@ -43,8 +47,37 @@ router.get("/establishment", async (req, res) => {
   }
 });
 
-// cRud Read (GET) (Detalhe)
-router.get("/establishment/:id", async (req, res) => {
+// cRud Read (GET) (Lista) - Admin
+router.get(
+  "/establishment/admin",
+  isAuthenticated,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const establishments = await EstablishmentModel.find()
+        .populate({
+          path: "products",
+          model: "Product",
+        })
+        .populate({
+          path: "reviews",
+          model: "Reviews",
+        })
+        .populate({
+          path: "orders",
+          model: "Order",
+        });
+
+      res.status(200).json(establishments);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+);
+
+// cRud Read (GET) (Detalhe) - if na order(admin) populate
+router.get("/establishment/:id", isAuthenticated, async (req, res) => {
   try {
     const establishment = await EstablishmentModel.findOne({
       _id: req.params.id,
@@ -61,46 +94,54 @@ router.get("/establishment/:id", async (req, res) => {
   }
 });
 
-
 // PUT => substituição (destrutiva)
 // PATCH => atualização (não-destrutiva)
 
-// crUd Update (PATCH)
-router.patch("/establishment/:id", async (req, res) => {
-  try {
+// crUd Update (PATCH) - só admin
+router.patch(
+  "/establishment/:id",
+  isAuthenticated,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const result = await EstablishmentModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: req.body },
+        { new: true, runValidators: true }
+      );
 
-    const result = await EstablishmentModel.findOneAndUpdate(
-      { _id: req.params.id },
-      { $set: req.body },
-      { new: true, runValidators: true }
-    );
+      if (!result) {
+        return res.status(404).json({ msg: "Estabelecimento não encontrado." });
+      }
 
-    if (!result) {
-      return res.status(404).json({ msg: "Estabelecimento não encontrado." });
+      res.status(200).json(result);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
   }
-});
+);
 
-// cruD Delete (DELETE)
+// cruD Delete (DELETE) - só admin
 
-router.delete("/establishment/:id", async (req, res) => {
-  try {
-    const result = await EstablishmentModel.deleteOne({ _id: req.params.id });
+router.delete(
+  "/establishment/:id",
+  isAuthenticated,
+  isAdmin,
+  async (req, res) => {
+    try {
+      const result = await EstablishmentModel.deleteOne({ _id: req.params.id });
 
-    if (result.deletedCount < 1) {
-      return res.status(404).json({ msg: "Estabelecimento não encontrado" });
+      if (result.deletedCount < 1) {
+        return res.status(404).json({ msg: "Estabelecimento não encontrado" });
+      }
+
+      res.status(200).json({});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-
-    res.status(200).json({});
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
   }
-});
+);
 
 module.exports = router;
